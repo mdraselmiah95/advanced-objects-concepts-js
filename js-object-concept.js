80ec267af1a90e8dd52176911639641da754bc0c
rasel <russelroy15@gmail.com>
2021-08-30 23:58:45 +0600

    learning object new away

--- js-object-concept.js
+++ js-object-concept.js
@@ -1,1 +1,35 @@
-console.log(999)
+//using object literal
+const student = {
+    name: 'Shakib al hasan',
+    job: 'cricketer'
+};
+
+//constructor
+
+const person = new Object();
+
+
+//3.
+
+const human = Object.create(student);
+// console.log(human.job);
+
+class People {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const peopleTwo = new People('Manus', 12);
+// console.log(peopleTwo);
+
+
+//function
+
+function Manus(name) {
+    this.name = name;
+}
+
+const man = new Manus('Asif');
+console.log(man)
